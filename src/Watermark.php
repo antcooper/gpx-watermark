0880d8937bca231d158fedb7195467a70b7e67f6
Ant Cooper <antjcooper@gmail.com>
2020-08-24 11:14:04 +0000

    Refactor message (de)coding into Class;

--- src/Watermark.php
+++ src/Watermark.php
@@ -3,7 +3,6 @@
 namespace antcooper\gpxwatermark;
 
 use antcooper\gpxwatermark\FileHandler;
-use antcooper\gpxwatermark\Payload;
 
 class Watermark
 {
@@ -26,7 +25,7 @@ class Watermark
         $files = $fileHandler->getManifest();
 
         // Create encoded payload
-        $encodedMessage = Payload::encode($watermark);
+        $encodedMessage = $this->encode($watermark);
 
 
         foreach($files as $file) {
@@ -98,7 +97,7 @@ class Watermark
         $encodedMessage = $this->exportEncodedMessageFromWaypoints($waypoints);
 
         // Decode message
-        $message = Payload::decode($encodedMessage);
+        $message = $this->decode($encodedMessage);
 
         // Split long message string into array
         $messages = explode('|',$message);
@@ -175,7 +174,7 @@ class Watermark
         }
 
         // Decode message
-        $message = Payload::decode($encodedMessage);
+        $message = $this->decode($encodedMessage);
 
         // Split long message string into array
         $messages = explode('|',$message);
@@ -296,7 +295,57 @@ class Watermark
         }
     }
 
+   /**
+     * Encode the payload into an array of 2 digit letter codes based on ASCII characterset
+     * 
+     * @param  string $message
+     * @param  string $delimiter
+     * @return array
+     */
+    private function encode($message, $delimiter = '||')
+    {
+        // Set delimiter on the end of message
+        $message = $message . $delimiter;
 
+        $payload = [];
+        // Loop over each character in the message
+        for($i=0; $i < strlen($message); $i++) {
+            // Get the character from string
+            $chr = substr($message, $i);
+
+            // Get the ASCII value and subtract 32
+            $chr = ord($chr) -32;
+
+            // Pad to two numbers
+            $chr = str_pad($chr, 2, "0", STR_PAD_LEFT);
+
+            // Add to message array
+            $payload[] = $chr;
+        }
+        return $payload;
+    }
+
+
+    /**
+     * Decode the numeric message into a string
+     * 
+     * @param  array $payload
+     * @return string
+     */
+    private function decode($payload)
+    {
+        $message = "";
+        foreach($payload as $letter) {
+            if ($letter) {
+                $message .= chr($letter+32);
+            }
+            else {
+                $message .= '*';
+            }
+        }
+
+        return $message;
+    }
 }
 
 
